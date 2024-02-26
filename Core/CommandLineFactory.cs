2ea224a15282a5d13ab7ad1e0c3a9608ca522243
顾天皓 <gutianhao@wondersgroup.com>
2024-02-26 17:45:10 +0800

    init

--- Core/CommandLineFactory.cs
+++ Core/CommandLineFactory.cs
@@ -3,8 +3,12 @@ namespace Core;
 
 public class CommandLineFactory
 {
+    private List<ICommand> _commands = [];
+
     public ICommand Generate(List<string> args)
     {
         return new SearchCommand();
     }
+
+    
 }
--- Core/Program.cs
+++ Core/Program.cs
@@ -1,9 +1,18 @@
-﻿internal class Program
+﻿using Core;
+
+internal class Program
 {
     //oj8api search String
     //oj8api add    
-    private static void Main(string[] args)
+    private static void Main(List<string> args)
     {
-        
+        CommandLineFactory factory = new();
+
+        factory.append(new SearchCommand());
+        factory.append(new AddCommand());
+
+        ICommand command = factory.Generate(args);
+
+        command.Execute(args)
     }
 }
